aae067d0150916471c1c8e0ddf270afcd425f99e
ahuber33 <ahuber33@users.noreply.github.com>
2025-08-22 11:11:33 +0000

    deploy: 39faba69ba398bffed2a371f596c390d26e1172c

--- search/all_e.js
+++ search/all_e.js
@@ -1,15 +1,14 @@
 var searchData=
 [
-  ['readme_0',['README',['../md_README.html',1,'']]],
-  ['readme_2emd_1',['README.md',['../README_8md.html',1,'']]],
-  ['resetdepositedenergy_2',['ResetDepositedEnergy',['../structRunTallyYAG.html#a7bca2ab3ac47e1203b1379bfa9e477ab',1,'RunTallyYAG']]],
-  ['resetflag_3',['resetflag',['../structRunTallyCollimators.html#a4245977c41704ceba13eae4f27f99c96',1,'RunTallyCollimators::ResetFlag()'],['../structRunTallyYAG.html#a5b7a0007c22cf580f367a2640cd67813',1,'RunTallyYAG::ResetFlag()']]],
-  ['returnflag_4',['ReturnFlag',['../structRunTallyYAG.html#a2b2dd64d3c159a015d40e34171f46f4e',1,'RunTallyYAG']]],
-  ['root_20output_5',['ROOT Output',['../index.html#autotoc_md28',1,'']]],
-  ['running_20the_20simulation_6',['Running the Simulation',['../index.html#autotoc_md16',1,'']]],
-  ['runtallycollimators_7',['RunTallyCollimators',['../structRunTallyCollimators.html',1,'']]],
-  ['runtallyglobalinput_8',['RunTallyGlobalInput',['../structRunTallyGlobalInput.html',1,'']]],
-  ['runtallyinput_9',['RunTallyInput',['../structRunTallyInput.html',1,'']]],
-  ['runtallyquadrupoles_10',['RunTallyQuadrupoles',['../structRunTallyQuadrupoles.html',1,'']]],
-  ['runtallyyag_11',['RunTallyYAG',['../structRunTallyYAG.html',1,'']]]
+  ['readme_2emd_0',['README.md',['../README_8md.html',1,'']]],
+  ['resetdepositedenergy_1',['ResetDepositedEnergy',['../structRunTallyYAG.html#a7bca2ab3ac47e1203b1379bfa9e477ab',1,'RunTallyYAG']]],
+  ['resetflag_2',['resetflag',['../structRunTallyCollimators.html#a4245977c41704ceba13eae4f27f99c96',1,'RunTallyCollimators::ResetFlag()'],['../structRunTallyYAG.html#a5b7a0007c22cf580f367a2640cd67813',1,'RunTallyYAG::ResetFlag()']]],
+  ['returnflag_3',['ReturnFlag',['../structRunTallyYAG.html#a2b2dd64d3c159a015d40e34171f46f4e',1,'RunTallyYAG']]],
+  ['root_20output_4',['ROOT Output',['../index.html#autotoc_md28',1,'']]],
+  ['running_20the_20simulation_5',['Running the Simulation',['../index.html#autotoc_md16',1,'']]],
+  ['runtallycollimators_6',['RunTallyCollimators',['../structRunTallyCollimators.html',1,'']]],
+  ['runtallyglobalinput_7',['RunTallyGlobalInput',['../structRunTallyGlobalInput.html',1,'']]],
+  ['runtallyinput_8',['RunTallyInput',['../structRunTallyInput.html',1,'']]],
+  ['runtallyquadrupoles_9',['RunTallyQuadrupoles',['../structRunTallyQuadrupoles.html',1,'']]],
+  ['runtallyyag_10',['RunTallyYAG',['../structRunTallyYAG.html',1,'']]]
 ];
